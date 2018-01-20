cd25d0d01c6633902c39c8c85cb59419eb43610e
php 20 <php.20@penadinho.cec.ime.usp.br>
2018-01-19 22:47:24 -0200

    Adição da classe Conta

--- conta.php
+++ conta.php
@@ -4,12 +4,15 @@ class Conta
 {
     public $numero; //Atributo
     public $saldo; //Atributo
+    public $limite; //Atributo
 
 }
 
 $x = new Conta();
-$x ->  numero = 0 ;
-$x ->  saldo = 125;
+$x ->  numero = "0";
+$x ->  saldo = "125";
+$x ->  limite =  "560,00"
 
-echo "Conta = ". $x -> numero ."\n" ;
-echo "Saldo = ". $x -> saldo  ."\n";
+echo "Conta  = ". $x -> numero  ."\n" ;
+echo "Saldo  = ". $x -> saldo   ."\n";
+echo "Limite = ". $x -> limite  ."\n";

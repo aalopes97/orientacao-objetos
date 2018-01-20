38fdb007308201edfe3016a974e44ed86018ac08
php 20 <php.20@penadinho.cec.ime.usp.br>
2018-01-19 22:32:37 -0200

    Adição de classe CLiente

---
+++ cliente.php
@@ -0,0 +1,23 @@
+<?php
+
+class Cliente
+{
+  public $nome;   //Atributo
+  public $codigo; //atributo
+}
+
+$c1 = new Cliente;
+$c2 = new Cliente;
+
+$c1 -> nome   = "Rafael Consentino";
+$c1 -> codigo = "1";
+
+$c2 -> nome   = "Jonas Hirata";
+$c2 -> codigo = "2";
+
+
+echo "\n**********\n";
+echo "Cliente 1 \n Nome = ".$c1->nome."\n Código = ".$c1->codigo."\n";
+echo "\n**********\n";
+echo "Cliente 2 \n Nome = ".$c2->nome."\n Código = ".$c2->codigo."\n";
+echo "\n**********\n";
